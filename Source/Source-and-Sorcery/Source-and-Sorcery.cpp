79c39b88d75a0f8c6513809bf4c750f40b55e03d
zack <zack@zackbradshaw.com>
2024-02-27 16:06:18 -0600

    feat: add cpp

---
+++ Source/Source-and-Sorcery/Source-and-Sorcery.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Source-and-Sorcery.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Source-and-Sorcery, "Source-and-Sorcery" );
